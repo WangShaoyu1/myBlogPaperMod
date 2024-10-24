4ddb29d1c382fbc5857dc802226caf019c7b1328
wy <18672293959@126.com>
2024-10-24 10:35:09 +0800

    update：1、增加并发控制

--- utils/juejin/crawleeFromEveryUrl/crawlee.js
+++ utils/juejin/crawleeFromEveryUrl/crawlee.js
@@ -6,10 +6,16 @@ log.debug('Setting up crawler.');
 
 const crawler = new PlaywrightCrawler({
     requestHandler: router,
-    maxConcurrency: 20,
-    minConcurrency: 2,
     maxRequestsPerCrawl: Infinity,
     requestHandlerTimeoutSecs: 200,
+    autoscaledPoolOptions: {
+        maxConcurrency: 5,
+        minConcurrency: 1,
+        desiredConcurrencyRatio: 0.9,  // 保持接近目标并发数
+        scaleUpStepRatio: 0.15,        // 并发增加步长
+        scaleDownStepRatio: 0.15,      // 并发减少步长
+        autoscaleIntervalSecs: 5       // 自动缩放的时间间隔
+    }
 })
 
 await crawler.run(['https://juejin.cn'])
